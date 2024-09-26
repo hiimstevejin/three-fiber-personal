b4653352a769628341ae95e7a7105e079e8e7977
hiimstevejin <jingle5695@gmail.com>
2024-09-26 18:16:43 -0400

    Hero-1-3dRoom

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -50,7 +50,7 @@ export default function Navbar() {
         </div>
       </div>
 
-      <div className={`nav-sidebar ${isOpen ? 'max-h-screen' : 'max-h-0'}`}>
+      <div className={`nav-sidebar ${isOpen ? "max-h-screen" : "max-h-0"}`}>
         <nav className="p-5">
           <NavItems />
         </nav>
