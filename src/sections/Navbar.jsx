3c92caca0ba5f4d807c7ecba244aab04f5a1414e
hiimstevejin <jingle5695@gmail.com>
2024-09-26 01:58:35 -0400

    Navbar-2

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -1,7 +1,26 @@
 import { useState } from "react";
+import { navLinks } from "../constants";
 
 export default function Navbar() {
+  function NavItems() {
+    return (
+      <ul className="nav-ul">
+        {navLinks.map(({ id, href, name }) => (
+          <li key={id} className="nav-li">
+            <a href={href} className="nav-li_a" onClick={() => {}}>
+              {name}
+            </a>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+  //state for toggle menu
   const [isOpen, setisOpen] = useState(true);
+
+  function toggleMenu() {
+    setisOpen((prevIsOpen) => !prevIsOpen);
+  }
 
   return (
     <header className="fixed top-0 left-0 right-0 z-50 bg-black/90">
@@ -13,7 +32,11 @@ export default function Navbar() {
           >
             Steve
           </a>
-          <button>
+          <button
+            onClick={toggleMenu}
+            className="text-neutral-400 hover:text-white focus:outline-none sm:hidden flex"
+            aria-label="Toggle menu"
+          >
             {" "}
             <img
               src={isOpen ? "assets/close.svg" : "assets/menu.svg"}
@@ -21,7 +44,16 @@ export default function Navbar() {
               className="w-6 h-6"
             />
           </button>
+          <nav className="sm:flex hidden">
+            <NavItems />
+          </nav>
         </div>
+      </div>
+
+      <div className={`nav-sidebar ${isOpen ? 'max-h-screen' : 'max-h-0'}`}>
+        <nav className="p-5">
+          <NavItems />
+        </nav>
       </div>
     </header>
   );
