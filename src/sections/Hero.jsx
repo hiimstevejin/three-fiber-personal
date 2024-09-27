7f112930401aead4344858461e1caa11013ee87d
hiimstevejin <jingle5695@gmail.com>
2024-09-26 20:07:59 -0400

    Hero-1-Shadows and target

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -4,6 +4,7 @@ import HackerRoom from "../components/HackerRoom";
 import { Suspense } from "react";
 import CanvasLoader from "./CanvasLoader";
 import { Leva, useControls } from "leva";
+import Target from "../components/Target";
 // import { useMediaQuery } from "react-responsive";
 
 export default function Hero() {
@@ -44,7 +45,13 @@ export default function Hero() {
       max: 10,
     },
   });
+  const targetControls = useControls("Target", {
+    targetX: { value: 1, min: -10, max: 10 },
+    targetY: { value: 1, min: -10, max: 10 },
+    targetZ: { value: 1, min: -10, max: 10 },
+  });
   // const isMobile = useMediaQuery({maxWidth: 768})
+
   return (
     <section className="min-h-screen w-full flex flex-col relative">
       <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
@@ -54,23 +61,20 @@ export default function Hero() {
         <p className="hero_tag text-gray_gradient ">Studying Front-End</p>
       </div>
       <div className="w-full h-full absolute inset-0">
-        <Leva />
-        <Canvas className="w-full h-full">
+        {/* <Leva /> */}
+        <Canvas className="w-full h-full mt-4">
           <Suspense fallback={<CanvasLoader />}>
             <PerspectiveCamera makeDefault position={[0, 0, 30]} />
             <HackerRoom
-              // scale={1}
-              // position={[0, 0, 0]}
-              // rotation={[0, 280, 0]}
-              position={[0, -3, -4]}
-              rotation={[0.4, -0.6, 0]}
+              position={[0, -3, -8]}
+              rotation={[0.4, -0.7, 0]}
               scale={1}
             />
             <group>
-
+              <Target position={[-8.0,-6.0,3.0]}/>
             </group>
-            <ambientLight intensity={1} />
-            <directionalLight position={[10, 10, 10]} intensity={0.5} />
+            <ambientLight color= "black" intensity={1} />
+            <directionalLight color="white" position={[-10,7,6]} intensity={4} castShadow/>
           </Suspense>
         </Canvas>
       </div>
