fd2859606f2da8403315a6725ef76a4030deedbd
hiimstevejin <jingle5695@gmail.com>
2025-04-21 02:23:15 -0400

    update: updated about page to recent

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react';
-import Globe from 'react-globe.gl';
+import { useState } from "react";
+import Globe from "react-globe.gl";
 
-import Button from '../components/Button.jsx';
+import Button from "../components/Button.jsx";
 
 const About = () => {
   const [hasCopied, setHasCopied] = useState(false);
 
   const handleCopy = () => {
-    navigator.clipboard.writeText(' jingle5965@gmail.com ');
+    navigator.clipboard.writeText(" jingle5965@gmail.com ");
     setHasCopied(true);
 
     setTimeout(() => {
@@ -20,12 +20,17 @@ const About = () => {
       <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full">
         <div className="col-span-1 xl:row-span-3">
           <div className="grid-container">
-            <img src="assets/grid1.png" alt="grid-1" className="w-full sm:h-[276px] h-fit object-contain" />
+            <img
+              src="assets/grid1.png"
+              alt="grid-1"
+              className="w-full sm:h-[276px] h-fit object-contain"
+            />
 
             <div>
               <p className="grid-headtext">Hi, I’m Steve Jin</p>
               <p className="grid-subtext">
-                Currently attending University of Virginia, studying computer science aspiring fullstack developer
+                Currently attending University of Virginia, studying computer
+                science aspiring fullstack developer
               </p>
             </div>
           </div>
@@ -33,13 +38,18 @@ const About = () => {
 
         <div className="col-span-1 xl:row-span-3">
           <div className="grid-container">
-            <img src="assets/grid2.png" alt="grid-2" className="w-full sm:h-[276px] h-fit object-contain" />
+            <img
+              src="assets/grid2.png"
+              alt="grid-2"
+              className="w-full sm:h-[276px] h-fit object-contain"
+            />
 
             <div>
               <p className="grid-headtext">Tech Stack</p>
               <p className="grid-subtext">
-                I specialize in a variety of languages, frameworks, and tools that allow me to build robust and scalable
-                applications
+                I specialize in Javascript frameworks such as React, Next.js,
+                Node.js, Typescript and familiar with libraries Tailwind, and
+                Three.js
               </p>
             </div>
           </div>
@@ -57,12 +67,26 @@ const About = () => {
                 showGraticules
                 globeImageUrl="//unpkg.com/three-globe/example/img/earth-day.jpg"
                 bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
-                labelsData={[{ lat: 38.033554, lng: -78.507980, text: 'Im here ', color: 'white', size: 15 }]}
+                labelsData={[
+                  {
+                    lat: 38.033554,
+                    lng: -78.50798,
+                    text: "Im here ",
+                    color: "white",
+                    size: 15,
+                  },
+                ]}
               />
             </div>
             <div>
-              <p className="grid-headtext">I speak three languages and I&apos;m open to work globally</p>
-              <p className="grid-subtext">I&apos;m based in Charlottesville,Virginia and open to remote work.</p>
+              <p className="grid-headtext">
+                I am fluent in English and Korean and I&apos;m open to work
+                globally
+              </p>
+              <p className="grid-subtext">
+                I&apos;m based in Charlottesville,Virginia and open to remote
+                work.
+              </p>
               <Button name="Contact Me" isBeam containerClass="w-full mt-10" />
             </div>
           </div>
@@ -70,12 +94,31 @@ const About = () => {
 
         <div className="xl:col-span-2 xl:row-span-3">
           <div className="grid-container">
-            <img src="assets/grid3.png" alt="grid-3" className="w-full sm:h-[266px] h-fit object-contain" />
+            <img
+              src="assets/grid3.png"
+              alt="grid-3"
+              className="w-full sm:h-[266px] h-fit object-contain"
+            />
 
-            <div className='mt-14'>
+            <div className="mt-14">
               <p className="grid-headtext">My Passion for Coding</p>
               <p className="grid-subtext">
-                I love solving problems and building things through code.
+                I love solving problems and building things through code. My
+                recent project includes{" "}
+                <a
+                  href="https://devpost.com/software/travel-log-7yckum"
+                  style={{ color: "blue", textDecoration: "underline" }}
+                >
+                  Travelog
+                </a>
+                {" "}
+                and{" "}
+                <a
+                  href="https://buttered-warbler-b55.notion.site/React-Component-Library-1cdbe6d87a8280d5ad5fde3ba66e3799?pvs=4"
+                  style={{ color: "blue", textDecoration: "underline" }}
+                >
+                  Component Lab
+                </a>
               </p>
             </div>
           </div>
@@ -92,8 +135,13 @@ const About = () => {
             <div className="space-y-2">
               <p className="grid-subtext text-center">Contact me</p>
               <div className="copy-container" onClick={handleCopy}>
-                <img src={hasCopied ? 'assets/tick.svg' : 'assets/copy.svg'} alt="copy" />
-                <p className="lg:text-2xl md:text-xl font-medium text-gray_gradient text-white">jingle5695@gmail.com</p>
+                <img
+                  src={hasCopied ? "assets/tick.svg" : "assets/copy.svg"}
+                  alt="copy"
+                />
+                <p className="lg:text-2xl md:text-xl font-medium text-gray_gradient text-white">
+                  jingle5695@gmail.com
+                </p>
               </div>
             </div>
           </div>
